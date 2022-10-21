1bb86ec34d8f49992cbedcaaa1ddb6bfe484da2f
quezia <quezia.emanuelly2003@gmail.com>
2022-10-21 18:51:11 -0300

    adicionando o evento aos botoes de abrir o modal

--- Matflix/public/js/edit_post_modal.js
+++ Matflix/public/js/edit_post_modal.js
@@ -1,13 +1,33 @@
 const fadeModal = document.getElementById("fadeModal");
 const botoes = document.getElementsByClassName("botao");
+const fechar = document.getElementsByClassName("fechar");
 
 const botaoModal = [...botoes].filter((el) => {
     return el.dataset.modal != null;
 });
 
+const modalCurrent = () => {
+    const modal = document.getElementsByClassName("modal-post");
+    const modalOpen = [...modal].filter((el) => {
+        return !el.classList.contains("hide");
+    });
+
+    return modalOpen[0];
+}
+
 const toggleModal = (id) => {
-    const modalOpen = document.getElementById(id);
-    modalOpen.classList.toggle("hide");
+    if(id == undefined)
+    {
+        const modalOpen = modalCurrent();
+        fadeModal.classList.toggle("hide");
+        modalOpen.classList.toggle("hide");
+    }
+    else
+    {
+        const modalOpen = document.getElementById(id);
+        modalOpen.classList.toggle("hide");
+        fadeModal.classList.toggle("hide");
+    }
 }
 
 [...botaoModal, fadeModal].forEach((el) => {
