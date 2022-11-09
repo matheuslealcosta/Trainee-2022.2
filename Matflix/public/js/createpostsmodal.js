7957f1acc11092ebdfb00cb420943ffaec247d2f
marlonruffo <ruffomarlon9@gmail.com>
2022-11-09 20:31:30 -0300

    Modal criacao de posts

--- Matflix/public/js/createpostsmodal.js
+++ Matflix/public/js/createpostsmodal.js
@@ -1,23 +1,25 @@
-const botaopublique = document.querySelector(".botaopublique");
-const botaocancelar = document.querySelector(".botaocancelar");
+const botaopublique = document.querySelector(".add");
+const botaocancelar = document.querySelector(".cancc");
 
 
 botaopublique.addEventListener('click',abrirmodal);
 botaocancelar.addEventListener('click',fecharmodal);
 
 function abrirmodal(){
+    console.log('alo')
     let modal = document.querySelector("#main");
     modal.style.display='block';
-    let sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = 'none';
+    let pagecontainer = document.querySelector(".page-container");
+    pagecontainer.style.display = 'none';
 
 }
 
 
 
 function fecharmodal(){
+    console.log('oi')
     let modal = document.querySelector("#main");
     modal.style.display='none';
-    let sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = 'block';
+    let pagecontainer = document.querySelector(".page-container");
+    pagecontainer.style.display = 'block';
 }
