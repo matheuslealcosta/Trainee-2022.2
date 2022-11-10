868236000885af73b937960ad3270cdb4cc86c4e
marlonruffo <ruffomarlon9@gmail.com>
2022-11-09 22:23:33 -0300

    arrumado

--- Matflix/public/js/createpostsmodal.js
+++ Matflix/public/js/createpostsmodal.js
@@ -1,7 +1,5 @@
 const fadeModal = document.getElementById("fadeModal");
 const botaoAbrir = document.getElementsByClassName("botao");
-const botaoFechar = document.getElementById("Cancel");
-
 const botoesModal = [...botaoAbrir].filter((el) =>{
     return el.dataset.modal != null
 });
@@ -10,7 +8,6 @@ const modalCurrent= () => {
 }
 const toggleModal= (id) => {
     const modalOpen = document.getElementById(id);
-    console.log("hiiii")
     modalOpen.classList.toggle("hide");
     fadeModal.classList.toggle("hide");
 }
@@ -18,9 +15,6 @@ const toggleModal= (id) => {
     el.addEventListener("click", () => toggleModal(el.dataset.modal));
 })
 
-botaoFechar.addEventListener("click", fechar())
 
-function fechar(){
-    botaoAbrir.className+= " ali";
-    fadeModal.className+= " ali";
-}
+
+
