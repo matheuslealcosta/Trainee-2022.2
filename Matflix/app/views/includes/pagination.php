0635f457c25c7894b30f6dfd3a3fc8bb172e6cb7
nicolassoam <nsm4456@gmail.com>
2022-11-26 14:32:15 -0300

    corrigido css da paginacao

--- Matflix/app/views/includes/pagination.php
+++ Matflix/app/views/includes/pagination.php
@@ -2,20 +2,20 @@
   <ul class="pagination justify-content-center">
 
     <li class="page-item">
-      <a class="page-link paginacao" href="?pagina=<?= $page > 1 ? $page-1 :1  ?>" aria-label="anterior">
-        <span aria-hidden="true">&laquo;</span>
+      <a class="page-link linkdapagina" href="?pagina=<?= $page > 1 ? $page-1 :1  ?>" aria-label="anterior">
+        <span aria-hidden="true" class="text-black">&laquo;</span>
         <span class="sr-only">Previous</span>
       </a>
     </li>
 
     <?php for($page_n = 1; $page_n <= $total_pages; $page_n++): ?>
 
-    <li class="page-item <?= $page_n == $page ? "active": ""?>"><a class="page-link paginacao" href="?pagina=<?=$page_n?>"><?=$page_n?></a></li>
+    <li class="page-item "><a class="page-link linkdapagina <?= $page_n == $page ? "ativo": ""?>" href="?pagina=<?=$page_n?>"><?=$page_n?></a></li>
 
     <?php endfor;?>
     <li class="page-item">
-      <a class="page-link paginacao" href="?pagina=<?= $page < $total_pages ? $page+1 : $total_pages?>" aria-label="proximo">
-        <span aria-hidden="true">&raquo;</span>
+      <a class="page-link linkdapagina" href="?pagina=<?= $page < $total_pages ? $page+1 : $total_pages?>" aria-label="proximo">
+        <span aria-hidden="true" class="text-black">&raquo;</span>
         <span class="sr-only">Next</span>
       </a>
     </li>
