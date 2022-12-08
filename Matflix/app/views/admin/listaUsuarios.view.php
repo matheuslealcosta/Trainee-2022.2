def8eebec6b9780b7855db27e7c717b463ce6f9e
quezia <quezia.emanuelly2003@gmail.com>
2022-12-08 20:56:28 -0300

    centralizando na tela os modais da lista de usuarios

--- Matflix/app/views/admin/listaUsuarios.view.php
+++ Matflix/app/views/admin/listaUsuarios.view.php
@@ -41,148 +41,151 @@
         <div class="btn-adicionar">
             <button  class="add botao " type="button" data-modal="modalAdd" >Adicionar Usuário</button>
         </div>
-        <div class="fade-modal hide" id="fadeModal"></div>
-        <div id="modalAdd" class="hide fech">
 
-        <!--Modal Criar Usuário-->
-        <div class="modal-header">
-            <h2>Criar novo usuário</h2>
-            <button1 id="close-modal">X</button1>
-        </div>
-        <div class="modal-body"></div>
-
-        <form class="modal-create" method="POST" action="/lista-usuarios/create">
-            <input type="text" class="input-modal-create" name="name" placeholder="Digite o nome de Usuário" autofocus>
-            <input type="text" class="input-modal-create" name="email" placeholder="Digite seu E-mail" autofocus>
-            <input type="password" class="input-modal-create" name="password" placeholder="Digite sua Senha">
-            <input type="submit" class="input-modal-create" value="Cadastrar">
-            
-        </form>
-        </div>
-        <!---->
-
-        
-        <!--início da tabela de usuários-->
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col" class="number">#</th>
-                    <th scope="col">Nome Completo</th>
-                    <th>E-mail</th>
-                    <th scope="col" class="title-acao">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php foreach($users as $user):?>
-                <tr>  
-                    <th scope="row" class="number"><?= $user->id; ?></th>
-                    <td><?= $user->name; ?></td>
-                    <td class="email"><?= $user->email; ?></td>
-                    <td>
-                        <div class="btn-acoes">
-                            <button type="button">
-                                <i class="fa fa-eye botao" aria-hidden="true" data-modal="modalShow-<?=$user->id?>"></i>
-                            </button>
-                            <button type="button">
-                                <i class="fa fa-trash-o botao" aria-hidden="true" data-modal="modalDelete-<?=$user->id?>"></i>
-                            </button>
-                            <button type="button">
-                                <i class="fa fa-pencil-square-o botao" aria-hidden="true" data-modal="modalEdit-<?=$user->id?>"></i>
-                            </button>
-                        </div>
-                    </td>
-                </tr>
-
-                    <!-- Modal Editar Usuário-->
-
-
-                <div class="modal-user hide d-flex mx-auto justify-content-center main fech" id="modalEdit-<?=$user->id?>">
-                        <img src="../../../public/assets/MatflixLogoText.png" class="img-fluid mx-auto d-block" alt="" id="logo">
-                        <h2 class="mb-4" id="title">Edição de usuário</h2>
-                        <form method="POST" action="/lista-usuarios/edit">
-                            <div class="form-group mb-3">
-                            <input type="hidden" value="<?=$user->id?>" name="id">
-                                <label for="novousuario" class="col-sm-auto col-form-label">Novo nome do usuário:</label>
-                                
-                                <input type="text" name="name" id="titulo" class="form-control" placeholder="<?=$user->name?>">
-                                
-                            </div>
-            
-                            <div class="form-group mb-3">
-                                <label for="novoemail" class="col-sm-auto col-form-label">Novo e-mail:</label>
-                                
-                                <input type="text" name="email" id="email" class="form-control" placeholder="<?=$user->email?>">
-                            </div>
-            
-                            <div class="form-group mb-3">
-                                <label for="novasenha" class="col-sm-auto col-form-label">Nova senha:</label>
-                                
-                                <input type="password"  value="" name="password" class="form-control"/>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-sm-6">
-                                    <input type="submit" value="Editar" class="btn btn-lg formbtn form-control">
-                                </div>
-                
-                                <div class="col-sm-6">
-                                    <input type="reset" value="Limpar" class="btn btn-lg formbtn form-control">
-                                </div>
-                
-                                <div class="botoes">
-                                    <input class="btn btn-lg formbtn form-control align-self-center col-sm-6 fechar" type="button" value="Cancelar">
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-            </div> 
-            <tr>
-                <!----MODAL EXCLUIR---->
-            <div class="modal-user hide d-flex mx-auto overflow-hidden justify-content-center main fech" id="modalDelete-<?=$user->id?>">
-                <img src="../../../public/assets/MatflixLogoText.png" class="img-fluid mx-auto d-block" alt="" id="logo">
-                <h2 id="title">Deleção de Usuario</h2>
-                <p class="lead text-center">Tem certeza que deseja excluir o usuário?</p>
-                    <div class="row d-flex justify-content-center">
-                        <form action="/lista-usuarios/delete" method="POST">
-                            <input type="hidden" value="<?=$user->id?>" name="id">
-                            <button type="submit" class="btn btn-lg formbtn col-md-4" >Sim</button>
-                            <button type="button" class="btn btn-lg formbtn col-md-4 fechar">Cancelar</button>
-                        </form>
-                    </div>
-                </div>
-            
-            <!---->
-            <!----MODAL VISUALIZAR--->
-            <div class="modal-user hide d-flex mx-auto overflow-hidden justify-content-center main fech"id="modalShow-<?=$user->id?>" >
-                <img src="../../../public/assets/MatflixLogoText.png" class="img-fluid mx-auto d-block" alt="" id="logo">
-                <h2 class="mb-4" id="title">Visualizar Usuário</h2>
-                <div class="row gy-3 gx-3 align-items-center mt-2">
-                        <div class="form-group mb-3">
-                            <label for="usuario" class="col-sm-auto col-form-label">Nome do usuário:</label>
-                            
-                            <p class=" form showusuario show"><?=$user->name?></p>
-                        
-                        </div>
-
-                        <div class="form-group mb-3">
-                            <label for="email" class="col-sm-auto col-form-label">E-mail:</label>
-                            
-                            <p class=" form showemail show"><?=$user->email?></p>
-                        </div>
-
-                        <div>
-                            <button class="btn btn-lg formbtn form-control align-self-center col-sm-6 fechar">Voltar</button>
-                        </div>
-            </div>
-                </div>
-                        <!----->
-                        
-            <!---->
-            </div>
+        <div class="container-modal">
+            <div class="fade-modal hide" id="fadeModal"></div>
+            <div id="modalAdd" class="hide fech">
     
-            <?php endforeach;?>
-            </tbody>
-          </table>
-        <!--fim da tabela de usuários-->
+            <!--Modal Criar Usuário-->
+            <div class="modal-header">
+                <h2>Criar novo usuário</h2>
+                <button1 id="close-modal">X</button1>
+            </div>
+            <div class="modal-body"></div>
+    
+            <form class="modal-create" method="POST" action="/lista-usuarios/create">
+                <input type="text" class="input-modal-create" name="name" placeholder="Digite o nome de Usuário" autofocus>
+                <input type="text" class="input-modal-create" name="email" placeholder="Digite seu E-mail" autofocus>
+                <input type="password" class="input-modal-create" name="password" placeholder="Digite sua Senha">
+                <input type="submit" class="input-modal-create" value="Cadastrar">
+                
+            </form>
+            </div>
+            <!---->
+    
+            
+            <!--início da tabela de usuários-->
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col" class="number">#</th>
+                        <th scope="col">Nome Completo</th>
+                        <th>E-mail</th>
+                        <th scope="col" class="title-acao">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach($users as $user):?>
+                    <tr>  
+                        <th scope="row" class="number"><?= $user->id; ?></th>
+                        <td><?= $user->name; ?></td>
+                        <td class="email"><?= $user->email; ?></td>
+                        <td>
+                            <div class="btn-acoes">
+                                <button type="button">
+                                    <i class="fa fa-eye botao" aria-hidden="true" data-modal="modalShow-<?=$user->id?>"></i>
+                                </button>
+                                <button type="button">
+                                    <i class="fa fa-trash-o botao" aria-hidden="true" data-modal="modalDelete-<?=$user->id?>"></i>
+                                </button>
+                                <button type="button">
+                                    <i class="fa fa-pencil-square-o botao" aria-hidden="true" data-modal="modalEdit-<?=$user->id?>"></i>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+    
+                        <!-- Modal Editar Usuário-->
+    
+    
+                    <div class="modal-user hide d-flex mx-auto justify-content-center main fech" id="modalEdit-<?=$user->id?>">
+                            <img src="../../../public/assets/MatflixLogoText.png" class="img-fluid mx-auto d-block" alt="" id="logo">
+                            <h2 class="mb-4" id="title">Edição de usuário</h2>
+                            <form method="POST" action="/lista-usuarios/edit">
+                                <div class="form-group mb-3">
+                                <input type="hidden" value="<?=$user->id?>" name="id">
+                                    <label for="novousuario" class="col-sm-auto col-form-label">Novo nome do usuário:</label>
+                                    
+                                    <input type="text" name="name" id="titulo" class="form-control" placeholder="<?=$user->name?>">
+                                    
+                                </div>
+                
+                                <div class="form-group mb-3">
+                                    <label for="novoemail" class="col-sm-auto col-form-label">Novo e-mail:</label>
+                                    
+                                    <input type="text" name="email" id="email" class="form-control" placeholder="<?=$user->email?>">
+                                </div>
+                
+                                <div class="form-group mb-3">
+                                    <label for="novasenha" class="col-sm-auto col-form-label">Nova senha:</label>
+                                    
+                                    <input type="password"  value="" name="password" class="form-control"/>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6">
+                                        <input type="submit" value="Editar" class="btn btn-lg formbtn form-control">
+                                    </div>
+                    
+                                    <div class="col-sm-6">
+                                        <input type="reset" value="Limpar" class="btn btn-lg formbtn form-control">
+                                    </div>
+                    
+                                    <div class="botoes">
+                                        <input class="btn btn-lg formbtn form-control align-self-center col-sm-6 fechar" type="button" value="Cancelar">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                </div> 
+                <tr>
+                    <!----MODAL EXCLUIR---->
+                <div class="modal-user hide d-flex mx-auto overflow-hidden justify-content-center main fech" id="modalDelete-<?=$user->id?>">
+                    <img src="../../../public/assets/MatflixLogoText.png" class="img-fluid mx-auto d-block" alt="" id="logo">
+                    <h2 id="title">Deleção de Usuario</h2>
+                    <p class="lead text-center">Tem certeza que deseja excluir o usuário?</p>
+                        <div class="row d-flex justify-content-center">
+                            <form action="/lista-usuarios/delete" method="POST">
+                                <input type="hidden" value="<?=$user->id?>" name="id">
+                                <button type="submit" class="btn btn-lg formbtn col-md-4" >Sim</button>
+                                <button type="button" class="btn btn-lg formbtn col-md-4 fechar">Cancelar</button>
+                            </form>
+                        </div>
+                    </div>
+                
+                <!---->
+                <!----MODAL VISUALIZAR--->
+                <div class="modal-user hide d-flex mx-auto overflow-hidden justify-content-center main fech"id="modalShow-<?=$user->id?>" >
+                    <img src="../../../public/assets/MatflixLogoText.png" class="img-fluid mx-auto d-block" alt="" id="logo">
+                    <h2 class="mb-4" id="title">Visualizar Usuário</h2>
+                    <div class="row gy-3 gx-3 align-items-center mt-2">
+                            <div class="form-group mb-3">
+                                <label for="usuario" class="col-sm-auto col-form-label">Nome do usuário:</label>
+                                
+                                <p class=" form showusuario show"><?=$user->name?></p>
+                            
+                            </div>
+    
+                            <div class="form-group mb-3">
+                                <label for="email" class="col-sm-auto col-form-label">E-mail:</label>
+                                
+                                <p class=" form showemail show"><?=$user->email?></p>
+                            </div>
+    
+                            <div>
+                                <button class="btn btn-lg formbtn form-control align-self-center col-sm-6 fechar">Voltar</button>
+                            </div>
+                </div>
+                    </div>
+                            <!----->
+                            
+                <!---->
+                </div>
+        
+                <?php endforeach;?>
+                </tbody>
+              </table>
+            <!--fim da tabela de usuários-->
+        </div>
         <?php require('app/views/includes/pagination.php');?>
        
        </div>  
